9af48ed1153f8fc3fcaf7dbf6132717f070e1225
Kilpivii <viivi.kilpiainen@gmail.com>
2023-03-27 02:26:14 +0300

    peli pohja koodattu 'tyhmällä' vastustajalla

---
+++ Koodi/src/kivi_sakset_paperi.py
@@ -0,0 +1,33 @@
+import random
+from vastustaja_tekoaly import Vastustaja
+from tulokset import Tulokset
+
+class KiviSaksetPaperi:
+
+    def kivi_sakset_paperi_peli(self):
+        tulokset = Tulokset()
+        vastustaja = Vastustaja()
+        print("KIVI, SAKSET, PAPERI!")
+
+        pelaaja = input("Valitse kivi (k), sakset (s) tai paperi (p), muilla lopettaa  ")
+        vastustajan_valinta = vastustaja.anna_valinta()
+        while self._peli_jatkuu(pelaaja):
+            print(f"{self.pelaajan_valinta(pelaaja)} vastaan {self.pelaajan_valinta(vastustajan_valinta)}")
+            
+            tulokset.tulos(pelaaja, vastustajan_valinta)
+            print(tulokset)
+            pelaaja = input("Valitse kivi (k), sakset (s) tai paperi (p), muilla lopettaa  ")
+            vastustajan_valinta = vastustaja.anna_valinta()
+          
+    def pelaajan_valinta(self, kirjain):
+        pelaajan_valinta = ""
+        if kirjain == "k":
+            pelaajan_valinta = "kivi"
+        elif kirjain == "s":
+            pelaajan_valinta = "sakset"
+        elif kirjain == "p":
+            pelaajan_valinta = "paperi"
+        return pelaajan_valinta
+
+    def _peli_jatkuu(self, siirto):
+        return siirto == "k" or siirto == "s" or siirto == "p"
---
+++ Koodi/src/vastustaja_tekoaly.py
@@ -0,0 +1,16 @@
+import random
+
+class Vastustaja:
+    def __init__(self):
+        self._valinta = 0
+
+    def anna_valinta(self):
+        self._valinta = random.randint(1, 3)
+
+        if self._valinta == 1:
+            return "k"
+        elif self._valinta == 2:
+            return "s"
+        else:
+            return "p"
+    
